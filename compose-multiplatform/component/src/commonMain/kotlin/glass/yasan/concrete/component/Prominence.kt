ac7ec032fc01e745132e48e9488ea8987cbd47f1
Yasan Glass <yasanglass@gmail.com>
2025-12-02 23:43:43 +0100

    Create Text composable component

---
+++ compose-multiplatform/component/src/commonMain/kotlin/glass/yasan/concrete/component/Prominence.kt
@@ -0,0 +1,10 @@
+package glass.yasan.concrete.component
+
+/**
+ * The prominence level of a component.
+ */
+public enum class Prominence {
+    HIGH,
+    NORMAL,
+    LOW,
+}
---
+++ compose-multiplatform/component/src/commonMain/kotlin/glass/yasan/concrete/component/Text.kt
@@ -0,0 +1,48 @@
+package glass.yasan.concrete.component
+
+import androidx.compose.material3.Text as Material3Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.font.FontStyle
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.text.style.TextOverflow
+import androidx.compose.ui.unit.TextUnit
+import glass.yasan.concrete.foundation.font.rubikMonoFontFamily
+import glass.yasan.concrete.foundation.theme.ConcreteTheme
+
+@Composable
+public fun Text(
+    text: String,
+    modifier: Modifier = Modifier,
+    prominence: Prominence = NORMAL,
+    fontSize: TextUnit = TextUnit.Unspecified,
+    fontStyle: FontStyle? = null,
+    fontWeight: FontWeight? = null,
+    textAlign: TextAlign? = null,
+    overflow: TextOverflow = TextOverflow.Clip,
+    softWrap: Boolean = true,
+    maxLines: Int = Int.MAX_VALUE,
+    minLines: Int = 1,
+) {
+    Material3Text(
+        text = text,
+        modifier = modifier,
+        color = when (prominence) {
+            LOW -> ConcreteTheme.content.minor
+            else -> ConcreteTheme.content.major
+        },
+        fontFamily = when (prominence) {
+            HIGH -> rubikMonoFontFamily()
+            else -> null
+        },
+        fontSize = fontSize,
+        fontStyle = fontStyle,
+        fontWeight = fontWeight,
+        textAlign = textAlign,
+        overflow = overflow,
+        softWrap = softWrap,
+        maxLines = maxLines,
+        minLines = minLines,
+    )
+}
