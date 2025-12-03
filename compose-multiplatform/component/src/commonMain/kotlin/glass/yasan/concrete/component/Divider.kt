7cf78ca7bd9f6bd9674ce03f4e2c9e15da74d9d6
Yasan Glass <yasanglass@gmail.com>
2025-12-03 21:53:13 +0100

    Create Divider component

---
+++ compose-multiplatform/component/src/commonMain/kotlin/glass/yasan/concrete/component/Divider.kt
@@ -0,0 +1,23 @@
+package glass.yasan.concrete.component
+
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.unit.Dp
+import glass.yasan.concrete.foundation.annotation.ExperimentalConcreteApi
+import glass.yasan.concrete.foundation.theme.ConcreteTheme
+import androidx.compose.material3.HorizontalDivider as Material3HorizontalDivider
+
+@ExperimentalConcreteApi
+@Composable
+public fun Divider(
+    modifier: Modifier = Modifier,
+    color: Color = ConcreteTheme.layer.background,
+    thickness: Dp = ConcreteTheme.dimensions.borderStrokeWidth,
+) {
+    Material3HorizontalDivider(
+        modifier = modifier,
+        thickness = thickness,
+        color = color,
+    )
+}
