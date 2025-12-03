8b032335d26b328f1f51318d4d7bf8cb422e2691
Yasan Glass <yasanglass@gmail.com>
2025-12-04 00:10:17 +0100

    Create ColorExtensions.kt

---
+++ compose-multiplatform/foundation/src/commonMain/kotlin/glass/yasan/concrete/foundation/color/ColorExtensions.kt
@@ -0,0 +1,16 @@
+package glass.yasan.concrete.foundation.color
+
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.luminance
+
+private const val CONTAINER_ALPHA = 0.5f
+private const val LUMINANCE_THRESHOLD = 0.5f
+
+public fun Color.toContainerColor(): Color = copy(alpha = CONTAINER_ALPHA)
+
+public fun Color.toContentColor(): Color =
+    if (luminance() > LUMINANCE_THRESHOLD) {
+        Color.Black
+    } else {
+        Color.White
+    }
