f48b8ef07d313e9c8220fd8aa8324d97106bc802
Yasan Glass <yasanglass@gmail.com>
2025-12-06 15:13:39 +0100

    Update SampleApp to showcase all colors

--- compose-multiplatform/sample/composeApp/src/commonMain/kotlin/glass/yasan/concrete/sample/SampleApp.kt
+++ compose-multiplatform/sample/composeApp/src/commonMain/kotlin/glass/yasan/concrete/sample/SampleApp.kt
@@ -1,13 +1,16 @@
 package glass.yasan.concrete.sample
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.ColumnScope
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.requiredHeight
+import androidx.compose.foundation.layout.safeContentPadding
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
@@ -15,6 +18,7 @@ import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import glass.yasan.concrete.component.TextMono
+import glass.yasan.concrete.component.border
 import glass.yasan.concrete.composeapp.generated.resources.Res
 import glass.yasan.concrete.composeapp.generated.resources.app_title
 import glass.yasan.concrete.foundation.theme.ConcreteTheme
@@ -28,48 +32,76 @@ fun SampleApp() {
         Box(
             contentAlignment = Alignment.Center,
             modifier = Modifier
-                .fillMaxSize()
-                .background(ConcreteTheme.colors.midground),
+                .background(ConcreteTheme.colors.midground)
+                .safeContentPadding()
+                .fillMaxSize(),
         ) {
             Column(
-                verticalArrangement = Arrangement.spacedBy(8.dp),
                 horizontalAlignment = Alignment.CenterHorizontally,
                 modifier = Modifier
-                    .fillMaxWidth()
+                    .fillMaxSize()
                     .background(ConcreteTheme.colors.foreground)
                     .padding(16.dp),
             ) {
-                ColorBox(ConcreteTheme.colors.primaryDark)
-                ColorBox(ConcreteTheme.colors.primary)
-                ColorBox(ConcreteTheme.colors.primaryLight)
-                TextMono(
-                    text = stringResource(Res.string.app_title).uppercase(),
-                    prominence = SUBTLE,
-                    fontSize = 32.sp,
-                )
                 TextMono(
                     text = stringResource(Res.string.app_title).uppercase(),
                     fontSize = 32.sp,
                 )
-                TextMono(
-                    text = stringResource(Res.string.app_title).uppercase(),
-                    prominence = SUBTLE,
-                    fontSize = 32.sp,
+                Spacer(Modifier.height(12.dp))
+                ColorsRow(
+                    arrayOf(
+                        ConcreteTheme.colors.primaryDark,
+                        ConcreteTheme.colors.primary,
+                        ConcreteTheme.colors.primaryLight
+                    )
                 )
-                ColorBox(ConcreteTheme.colors.secondaryLight)
-                ColorBox(ConcreteTheme.colors.secondary)
-                ColorBox(ConcreteTheme.colors.secondaryDark)
+                ColorsRow(
+                    arrayOf(
+                        ConcreteTheme.colors.secondaryDark,
+                        ConcreteTheme.colors.secondary,
+                        ConcreteTheme.colors.secondaryLight
+                    )
+                )
+                ColorsRow(
+                    arrayOf(
+                        ConcreteTheme.colors.tertiaryDark,
+                        ConcreteTheme.colors.tertiary,
+                        ConcreteTheme.colors.tertiaryLight
+                    )
+                )
+                ColorsRow(
+                    arrayOf(
+                        ConcreteTheme.colors.content,
+                        ConcreteTheme.colors.contentSubtle,
+                    )
+                )
+                ColorsRow(
+                    arrayOf(
+                        ConcreteTheme.colors.foreground,
+                        ConcreteTheme.colors.midground,
+                        ConcreteTheme.colors.background,
+                    )
+                )
             }
         }
     }
 }
 
 @Composable
-private fun ColorBox(color: Color) {
-    Box(
-        Modifier
-            .fillMaxWidth()
-            .requiredHeight(64.dp)
-            .background(color)
-    )
+private fun ColumnScope.ColorsRow(colors: Array<Color>) {
+    Row(
+        modifier = Modifier
+            .weight(1f),
+    ) {
+        colors.forEach { color ->
+            Box(
+                Modifier
+                    .weight(1f)
+                    .fillMaxHeight()
+                    .background(color)
+                    .border(color = ConcreteTheme.colors.background),
+            )
+        }
+    }
 }
+
