9e31a19066f0985e2c81c41d88f2ddad3cd8f617
Yasan Ghaffarian <yasanglass@gmail.com>
2025-12-09 20:03:00 +0100

    Update sample to include more components

--- compose-multiplatform/sample/composeApp/src/commonMain/kotlin/glass/yasan/concrete/sample/SampleApp.kt
+++ compose-multiplatform/sample/composeApp/src/commonMain/kotlin/glass/yasan/concrete/sample/SampleApp.kt
@@ -3,27 +3,28 @@ package glass.yasan.concrete.sample
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Box
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.ColumnScope
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.safeContentPadding
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.MutableState
 import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.remember
+import androidx.compose.runtime.saveable.rememberSaveable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.style.TextAlign
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
+import glass.yasan.concrete.component.HorizontalDivider
+import glass.yasan.concrete.component.Slider
 import glass.yasan.concrete.component.Switch
 import glass.yasan.concrete.component.Text
 import glass.yasan.concrete.component.TextMono
@@ -39,40 +40,59 @@ import org.jetbrains.compose.ui.tooling.preview.Preview
 @Composable
 fun SampleApp() {
     val isSystemInDarkTheme = isSystemInDarkTheme()
-    val isDarkTheme = remember { mutableStateOf(isSystemInDarkTheme) }
-
     val isDynamicAccentSupported = isDynamicAccentSupported()
-    val isDynamicAccentAllowed = remember { mutableStateOf(isDynamicAccentSupported) }
+
+    val isDarkTheme = rememberSaveable { mutableStateOf(isSystemInDarkTheme) }
+    val isDynamicAccentAllowed = rememberSaveable { mutableStateOf(isDynamicAccentSupported) }
+    val sliderValue = rememberSaveable { mutableStateOf(0.5f) }
 
     ConcreteTheme(
         isDark = isDarkTheme.value,
         isDynamicAccentAllowed = isDynamicAccentAllowed.value,
     ) {
-        Box(
-            contentAlignment = Alignment.Center,
+        LazyColumn(
+            verticalArrangement = Arrangement.spacedBy(8.dp),
+            horizontalAlignment = Alignment.CenterHorizontally,
             modifier = Modifier
-                .background(ConcreteTheme.colors.midground)
+                .background(ConcreteTheme.colors.foreground)
                 .safeContentPadding()
-                .fillMaxSize(),
+                .fillMaxSize()
+                .padding(16.dp),
         ) {
-            Column(
-                horizontalAlignment = Alignment.CenterHorizontally,
-                modifier = Modifier
-                    .fillMaxSize()
-                    .background(ConcreteTheme.colors.foreground)
-                    .padding(16.dp),
-            ) {
+            item {
                 TextMono(
                     text = stringResource(Res.string.app_title).uppercase(),
                     fontSize = 32.sp,
                 )
-                Spacer(Modifier.height(12.dp))
-                ColorPalette()
-                Spacer(Modifier.height(12.dp))
-                DarkThemeSwitch(isDarkTheme)
+            }
+            item { Spacer(Modifier.height(12.dp)) }
+            item { ColorPaletteContent() }
+            item { Spacer(Modifier.height(12.dp)) }
+            item { DarkThemeSwitch(isDarkTheme) }
+            item { HorizontalDivider() }
+            item {
                 DynamicAccentColorsSwitch(
                     isDynamicAccentAllowed = isDynamicAccentAllowed,
                     isDynamicAccentSupported = isDynamicAccentSupported,
+                )
+            }
+            item { HorizontalDivider() }
+            item { Spacer(Modifier.height(12.dp)) }
+            item {
+                Row(
+                    verticalAlignment = Alignment.CenterVertically,
+                    modifier = Modifier.fillMaxWidth(),
+                ) {
+                    Text(text = "Slider")
+                    Spacer(Modifier.weight(1f))
+                    Text(text = sliderValue.value.toString())
+                }
+            }
+            item {
+                Slider(
+                    value = sliderValue.value,
+                    onValueChange = { sliderValue.value = it },
+                    steps = 9,
                 )
             }
         }
@@ -86,7 +106,7 @@ private data class ColorItem(
 )
 
 @Composable
-private fun ColumnScope.ColorPalette() {
+private fun ColorPaletteContent() {
     ColorsRow(
         listOf(
             ColorItem(ConcreteTheme.colors.primaryHigh, ConcreteTheme.colors.onPrimaryHigh, "primaryHigh"),
@@ -170,17 +190,18 @@ private fun DynamicAccentColorsSwitch(
 }
 
 @Composable
-private fun ColumnScope.ColorsRow(colors: List<ColorItem>) {
+private fun ColorsRow(colors: List<ColorItem>) {
     Row(
         modifier = Modifier
-            .weight(1f),
+            .fillMaxWidth()
+            .height(100.dp),
     ) {
         colors.forEach { item ->
             Box(
                 contentAlignment = Alignment.Center,
                 modifier = Modifier
                     .weight(1f)
-                    .fillMaxHeight()
+                    .height(100.dp)
                     .background(item.color)
                     .border(color = ConcreteTheme.colors.background),
             ) {
