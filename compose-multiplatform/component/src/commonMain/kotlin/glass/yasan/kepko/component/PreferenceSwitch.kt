67dbb3fff8958640e69a0129ab92821d17ae9971
Yasan Ghaffarian <yasanglass@gmail.com>
2025-12-14 13:13:31 +0100

    Use PreferenceContainer in PreferenceSwitch

--- compose-multiplatform/component/src/commonMain/kotlin/glass/yasan/kepko/component/PreferenceSwitch.kt
+++ compose-multiplatform/component/src/commonMain/kotlin/glass/yasan/kepko/component/PreferenceSwitch.kt
@@ -1,21 +1,13 @@
 package glass.yasan.kepko.component
 
 import androidx.compose.foundation.background
-import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.runtime.Composable
-import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.painter.Painter
-import androidx.compose.ui.text.font.FontWeight
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import glass.yasan.kepko.foundation.annotation.ExperimentalKepkoApi
 import glass.yasan.kepko.foundation.theme.KepkoTheme
 import org.jetbrains.compose.resources.painterResource
@@ -62,140 +54,29 @@ public fun PreferenceSwitch(
     leadingContent: @Composable () -> Unit = {},
     annotation: PreferenceAnnotation? = null,
 ) {
-    val titleContentColor = if (enabled) KepkoTheme.colors.content else KepkoTheme.colors.contentDisabled
-    val descriptionContentColor = if (enabled) KepkoTheme.colors.contentSubtle else KepkoTheme.colors.contentDisabled
-    val shape = RoundedCornerShape(32.dp)
-
-    Column(
-        modifier = modifier
-            .border(color = KepkoTheme.colors.background, shape)
-            .clip(shape)
-            .clickable(
-                enabled = enabled,
-                onClick = { onCheckedChange(!checked) },
-            )
-            .background(KepkoTheme.colors.foreground)
-            .padding(
-                vertical = 8.dp,
-                horizontal = 24.dp,
-            ),
-    ) {
-        Row(
-            horizontalArrangement = Arrangement.spacedBy(12.dp),
-            verticalAlignment = Alignment.CenterVertically,
-            modifier = Modifier.fillMaxWidth(),
-        ) {
-            leadingContent()
-            Column(
-                verticalArrangement = Arrangement.spacedBy(4.dp),
-                modifier = Modifier.weight(1f),
-            ) {
-                Text(
-                    text = title,
-                    color = titleContentColor,
-                    fontWeight = FontWeight.Medium,
-                    fontSize = 16.sp,
-                )
-                description?.let {
-                    Text(
-                        text = it,
-                        color = descriptionContentColor,
-                        lineHeight = 16.sp,
-                        fontSize = 12.sp,
-                    )
-                }
-                annotation?.let {
-                    Pill(
-                        text = it.text(),
-                        containerColor = it.containerColor(),
-                        contentColor = it.contentColor(),
-                        modifier = Modifier.padding(vertical = 4.dp),
-                    )
-                }
-            }
+    PreferenceContainer(
+        title = title,
+        description = description,
+        onClick = { onCheckedChange(!checked) },
+        enabled = enabled,
+        annotation = annotation,
+        modifier = modifier,
+        leadingContent = leadingContent,
+        trailingContent = {
             Switch(
                 enabled = enabled,
                 checked = checked,
                 onCheckedChange = onCheckedChange,
             )
         }
-    }
+    )
 }
 
 @OptIn(ExperimentalKepkoApi::class)
 @Preview
 @Composable
 private fun PreferenceSwitchPreview() {
-    val checked = arrayOf(true, false)
-    val enabled = arrayOf(true, false)
-    val descriptions = arrayOf(
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
-        null
-    )
-
-    Column {
-        KepkoTheme {
-            Column(
-                verticalArrangement = Arrangement.spacedBy(4.dp),
-                modifier = Modifier.background(KepkoTheme.colors.midground),
-            ) {
-                descriptions.forEach { description ->
-                    checked.forEach { isChecked ->
-                        enabled.forEach { isEnabled ->
-                            PreferenceSwitch(
-                                title = if (isChecked) "Checked" else "Unchecked",
-                                description = description,
-                                checked = isChecked,
-                                enabled = isEnabled,
-                                onCheckedChange = {},
-                                modifier = Modifier.padding(horizontal = 16.dp),
-                            )
-                        }
-                    }
-                }
-            }
-        }
-    }
-}
-
-@OptIn(ExperimentalKepkoApi::class)
-@Preview
-@Composable
-private fun PreferenceSwitchWithIconPreview() {
-    val checked = arrayOf(true, false)
-    val descriptions = arrayOf(
-        "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
-        null
-    )
-
-    Column {
-        KepkoTheme {
-            Column(
-                verticalArrangement = Arrangement.spacedBy(4.dp),
-                modifier = Modifier.background(KepkoTheme.colors.midground),
-            ) {
-                descriptions.forEach { description ->
-                    checked.forEach { isChecked ->
-                        PreferenceSwitch(
-                            title = if (isChecked) "Checked" else "Unchecked",
-                            description = description,
-                            checked = isChecked,
-                            onCheckedChange = {},
-                            leadingIcon = painterResource(Res.drawable.ic_asterisk),
-                            modifier = Modifier.padding(horizontal = 16.dp),
-                        )
-                    }
-                }
-            }
-        }
-    }
-}
-
-@OptIn(ExperimentalKepkoApi::class)
-@Preview
-@Composable
-private fun PreferenceSwitchWithLabelPreview() {
-    val checked = arrayOf(true, false)
+    val annotations = arrayOf(PreferenceAnnotation.new, null)
     val descriptions = arrayOf(
         "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
         null
@@ -204,16 +85,19 @@ private fun PreferenceSwitchWithLabelPreview() {
     KepkoTheme {
         Column(
             verticalArrangement = Arrangement.spacedBy(4.dp),
-            modifier = Modifier.background(KepkoTheme.colors.midground),
+            modifier = Modifier
+                .background(KepkoTheme.colors.midground)
+                .padding(vertical = 16.dp),
         ) {
-            descriptions.forEach { description ->
-                checked.forEach { isChecked ->
+            annotations.forEach { annotation ->
+                descriptions.forEach { description ->
                     PreferenceSwitch(
-                        title = if (isChecked) "Checked" else "Unchecked",
+                        title = "Preference Switch",
                         description = description,
-                        checked = isChecked,
+                        checked = true,
+                        enabled = true,
                         onCheckedChange = {},
-                        annotation = PreferenceAnnotation.experimental,
+                        annotation = annotation,
                         leadingIcon = painterResource(Res.drawable.ic_asterisk),
                         modifier = Modifier.padding(horizontal = 16.dp),
                     )
