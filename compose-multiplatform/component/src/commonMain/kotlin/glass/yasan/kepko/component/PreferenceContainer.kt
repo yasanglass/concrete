f87dde5668ab2821877b22bc89755d3d30e30125
Yasan Ghaffarian <yasanglass@gmail.com>
2025-12-14 13:13:13 +0100

    Create PreferenceContainer

---
+++ compose-multiplatform/component/src/commonMain/kotlin/glass/yasan/kepko/component/PreferenceContainer.kt
@@ -0,0 +1,120 @@
+package glass.yasan.kepko.component
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+import glass.yasan.kepko.foundation.annotation.ExperimentalKepkoApi
+import glass.yasan.kepko.foundation.theme.KepkoTheme
+import org.jetbrains.compose.ui.tooling.preview.Preview
+
+@OptIn(ExperimentalKepkoApi::class)
+@Composable
+public fun PreferenceContainer(
+    title: String,
+    modifier: Modifier = Modifier,
+    onClick: () -> Unit = {},
+    description: String? = null,
+    enabled: Boolean = true,
+    leadingContent: @Composable () -> Unit = {},
+    trailingContent: @Composable () -> Unit = {},
+    annotation: PreferenceAnnotation? = null,
+) {
+    val titleContentColor = if (enabled) KepkoTheme.colors.content else KepkoTheme.colors.contentDisabled
+    val descriptionContentColor = if (enabled) KepkoTheme.colors.contentSubtle else KepkoTheme.colors.contentDisabled
+    val shape = RoundedCornerShape(32.dp)
+
+    Column(
+        modifier = modifier
+            .border(color = KepkoTheme.colors.background, shape)
+            .clip(shape)
+            .clickable(
+                enabled = enabled,
+                onClick = onClick,
+            )
+            .background(KepkoTheme.colors.foreground)
+            .padding(
+                vertical = 8.dp,
+                horizontal = 24.dp,
+            ),
+    ) {
+        Row(
+            horizontalArrangement = Arrangement.spacedBy(12.dp),
+            verticalAlignment = Alignment.CenterVertically,
+            modifier = Modifier.fillMaxWidth(),
+        ) {
+            leadingContent()
+            Column(
+                verticalArrangement = Arrangement.spacedBy(4.dp),
+                modifier = Modifier.weight(1f),
+            ) {
+                Text(
+                    text = title,
+                    color = titleContentColor,
+                    fontWeight = FontWeight.Medium,
+                    fontSize = 16.sp,
+                )
+                description?.let {
+                    Text(
+                        text = it,
+                        color = descriptionContentColor,
+                        lineHeight = 16.sp,
+                        fontSize = 12.sp,
+                    )
+                }
+                annotation?.let {
+                    Pill(
+                        text = it.text(),
+                        containerColor = it.containerColor(),
+                        contentColor = it.contentColor(),
+                        modifier = Modifier.padding(vertical = 4.dp),
+                    )
+                }
+            }
+            trailingContent()
+        }
+    }
+}
+
+@OptIn(ExperimentalKepkoApi::class)
+@Preview
+@Composable
+private fun PreferenceContainerPreview() {
+    val annotations = arrayOf(PreferenceAnnotation.experimental, null)
+    val descriptions = arrayOf(
+        "Lorem ipsum dolor sit amet, consectetur adipiscing elit.",
+        null
+    )
+
+    KepkoTheme {
+        Column(
+            verticalArrangement = Arrangement.spacedBy(4.dp),
+            modifier = Modifier
+                .background(KepkoTheme.colors.midground)
+                .padding(vertical = 16.dp),
+        ) {
+            annotations.forEach { annotation ->
+                descriptions.forEach { description ->
+                    PreferenceContainer(
+                        title = "Preference Container",
+                        description = description,
+                        enabled = true,
+                        annotation = annotation,
+                        modifier = Modifier.padding(horizontal = 16.dp),
+                    )
+                }
+            }
+        }
+    }
+}
